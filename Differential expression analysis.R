7e4a849f91f90c396cb52dc7d6024f525fcb25e2
Yuwei <v351722260@outlook.com>
2025-02-26 01:00:18 +0100

    Differential expression analysis.R

--- Differential expression analysis.R
+++ Differential expression analysis.R
@@ -1,7 +1,7 @@
 # Load necessary libraries
 library(DESeq2)
 
-# Extract results for a pairwise comparison (e.g., comparing "Lung_WT_Case" vs. "Lung_WT_Control")
+# Extract results for a pairwise comparison
 results_lung <- results(dds, contrast = c("condition", "Lung_WT_Case", "Lung_WT_Control"))
 
 # Filter significant genes (padj < 0.05 and absolute log2FoldChange greater than 1)
@@ -14,12 +14,12 @@ significant_genes_lung <- results_lung[
 upregulated_genes <- subset(significant_genes_lung, log2FoldChange > 0)
 downregulated_genes <- subset(significant_genes_lung, log2FoldChange < 0)
 
-# Count the number of upregulated genes
+# Print the number of upregulated genes
 cat("Number of upregulated genes:", nrow(upregulated_genes), "\n")
-# Count the number of downregulated genes
+# Print the number of downregulated genes
 cat("Number of downregulated genes:", nrow(downregulated_genes), "\n")
 
-# Extract normalized expression data for genes of interest (e.g., Gbp2, Fcer1g, Tgfp1)
+# Extract normalized expression data for genes of interest 
 genes_of_interest <- c("Gbp2", "Fcer1g", "Tgfp1")
 vsd_subset <- vsd[genes_of_interest, ]
 
